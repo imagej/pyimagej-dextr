9f79e9e2b4fb74795dd8acb8e2b03afce35d7c3a
Edward Evans <echnoviru@gmail.com>
2021-05-27 08:49:16 -0500

    Add crop_image function

--- torchij/util/__init__.py
+++ torchij/util/__init__.py
@@ -26,3 +26,34 @@ def create_bounding_box(image, points=None, pad=0, zero_pad=False):
     y_max = min(inds[0].max() + pad, y_max_bound)
 
     return x_min, y_min, x_max, y_max
+
+def crop_image(image, bounding_box, zero_pad=False):
+    # get image bounds
+    bounds = (0, 0, image.shape[1] - 1, image.shape[0] -1)
+
+    # create valid bounding box (x_min, y_min, x_max, y_max)
+    bounding_box_valid = (max(bounding_box[0], bounds[0]),
+                          max(bounding_box[1], bounds[1]), 
+                          min(bounding_box[2], bounds[2]), 
+                          min(bounding_box[3], bounds[3]))
+
+    if zero_pad:
+        # initialize crop size with first 2 dimensions
+        crop = np.zeros((bounding_box[3] - bounding_box[1] + 1, bounding_box[2] - bounding_box[0] + 1), dtype=image.dtype)
+        offsets = (-bounding_box[0], -bounding_box[1])
+    else:
+        assert(bounding_box == bounding_box_valid)
+        crop = np.zeros((bounding_box_valid[3] - bounding_box_valid[1] + 1, bounding_box_valid[2] - bounding_box_valid[0] + 1), dtype=image.dtype)
+        offsets = (-bounding_box_valid[0], -bounding_box_valid[1])
+
+    # not sure what this bit does:
+    inds = tuple(map(sum, zip(bounding_box_valid, offsets + offsets)))
+
+    image = np.squeeze(image)
+    if image.ndim == 2:
+        crop[inds[1]:inds[3] + 1, inds[0]:inds[2] + 1] = image[bounding_box_valid[1]:bounding_box_valid[3] + 1, bounding_box_valid[0]:bounding_box_valid[2] + 1]
+    else:
+        crop = np.tile(crop[:, :, np.newaxis], [1, 1, 3]) # add 3 rgb channels
+        crop[inds[1]:inds[3] + 1, inds[0]:inds[2] + 1, :] = image[bounding_box_valid[1]:bounding_box_valid[3] + 1, bounding_box_valid[0]:bounding_box_valid[2] + 1, :]
+
+    return crop
