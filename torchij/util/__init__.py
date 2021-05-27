660d55ae318f67bc9a012e4f8f28029e9120edb1
Edward Evans <echnoviru@gmail.com>
2021-05-27 08:38:16 -0500

    Add create_bounding_box function

--- torchij/util/__init__.py
+++ torchij/util/__init__.py
@@ -1,5 +1,28 @@
 import numpy as np
 
-def create_bounding_box():
+def create_bounding_box(image, points=None, pad=0, zero_pad=False):
+    if points is not None:
+        inds = np.flip(points.transpose(), axis=0)
+    else:
+        inds = np.where(image > 0)
 
-    return
+    if inds[0].shape[0] == 0:
+        return None
+        
+    if zero_pad:
+        x_min_bound = -np.inf
+        y_min_bound = -np.inf
+        x_max_bound = np.inf
+        y_max_bound = np.inf
+    else:
+        x_min_bound = 0
+        y_min_bound = 0
+        x_max_bound = image.shape[1] - 1
+        y_max_bound = image.shape[0] - 1
+
+    x_min = max(inds[1].min() - pad, x_min_bound)
+    y_min = max(inds[0].min() - pad, y_min_bound)
+    x_max = min(inds[1].max() + pad, x_max_bound)
+    y_max = min(inds[0].max() + pad, y_max_bound)
+
+    return x_min, y_min, x_max, y_max
