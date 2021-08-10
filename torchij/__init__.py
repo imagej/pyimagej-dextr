8cdaeda39be2027aed187149f296b926996b1c30
Edward Evans <echnoviru@gmail.com>
2021-08-10 10:48:02 -0500

    Add new functions to import masks into imagej
    join_mask_to_image will take result masks from the
    neural network and join them to the original image
    as an imagej stack.

--- torchij/__init__.py
+++ torchij/__init__.py
@@ -112,3 +112,80 @@ def collect_extreme_points_ori(points=4, timeout=0):
     extreme_points_ori = np.array(plt.ginput(points,timeout=timeout)).astype(np.int)
 
     return extreme_points_ori
+
+def mask_to_dataset(mask, ij_instance, show=False):
+    """
+    Return mask as a dataset.
+    :param mask: Boolean numpy array
+    """
+    if mask.dtype == bool:
+        mask = mask.astype(int)
+        
+    mask_ds = ij_instance.py.to_dataset(mask)
+    if show:
+        ij_instance.ui().show(mask_ds)
+    
+    return mask_ds
+
+def mask_to_xarray(mask, ij_instance, show=False):
+    """
+    Return mask as xarray DataArray.
+    """
+    if mask.dtype == bool:
+        mask = mask.astype(int)
+
+    mask_ds = ij_instance.py.to_dataset(mask)
+    if show:
+        ij_instance.ui().show(mask_ds)
+
+    mask_xr = ij_instance.py.from_java(mask_ds)
+
+    return mask_xr
+
+def join_mask_to_image(image, mask, ij_instance, show=False):
+    """
+    Take the result mask or masks from the neural network and return an imagej stack.
+    Returns joined Dataset.
+    """
+    if isinstance(mask, np.ndarray):
+        mask = mask_to_dataset(mask, ij_instance)
+        mask = ij_instance.py.from_java(mask)
+
+        if len(mask.dims) <= 2:
+            mask = mask.expand_dims('Channel')
+
+        joined_image = xr.concat([image[:,:,:], mask[:,:,:]], dim='Channel')
+        joined_image = ij_instance.py.to_dataset(joined_image)
+
+        if show:
+            ij_instance.ui().show(joined_image)
+    elif isinstance(mask, list):
+        if len(mask) <= 1:
+            single_mask = mask[0]
+            single_mask = mask_to_xarray(single_mask, ij_instance)
+            
+            if len(single_mask.dims) <= 2:
+                single_mask = single_mask.expand_dims('Channel')
+
+            joined_image = xr.concat([image[:,:,:], single_mask[:,:,:,]], dim='Channel')
+            joined_image = ij_instance.py.to_dataset(joined_image)
+        elif len(mask) >=2:
+            mask_list_ds = []
+            for m in mask: # convert numpy list to net.imagej.Dataset list
+                m = mask_to_xarray(m, ij_instance)
+                if len(m.dims) <= 2:
+                    m = m.expand_dims('Channel')
+                mask_list_ds.append(m)
+            
+            mask_count = len(mask_list_ds)
+            temp_mask = mask_list_ds[0]
+            for index in range(1, mask_count): # Dorky way to "append" masks to the input image
+                temp_mask = xr.concat([temp_mask[:,:,:], mask_list_ds[index][:,:,:]], dim='Channel')
+
+            joined_image = xr.concat([image[:,:,:], temp_mask[:,:,:]], dim='Channel')
+            joined_image = ij_instance.py.to_dataset(joined_image)
+        
+        if show:
+            ij_instance.ui().show(joined_image)
+
+    return joined_image
