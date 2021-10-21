1af10d4935191d47f161a3e9be90887e036d717e
Edward Evans <elevans2@wisc.edu>
2021-10-21 10:51:10 -0500

    Add setup python file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='net_segment',
+        version='0.0.1.dev0',
+        author='Edward Evans',
+        author_email='elevans2@wisc.edu',
+        packages=find_packages(),
+        platforms=['any'],
+        description='A collection of useful tools.',
+        )
+        
